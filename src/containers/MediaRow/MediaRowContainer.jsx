1d96c39e79c593353e98363d5f01c4f3388c2bec
Jaleel Bennett <>
2022-04-09 21:15:21 -0400

    added states to row context provider

--- src/containers/MediaRow/MediaRowContainer.jsx
+++ src/containers/MediaRow/MediaRowContainer.jsx
@@ -5,46 +5,46 @@ import './MediaRow.scss';
 
 const MediaRowContainer = ({title, medias, netflixOriginal, fullscreenProps, typeMedia, className,disableHover}) => {
 
-    const[isHovered, setIsHovered] = useState(false);
-    const [cardPopupWidth, setCardPopupWidth] = useState(''); 
-    const [cardPopupHeight, setCardPopupHeight] = useState('');
-    const [distance, setDistance] = useState(0);
-    const [totalPostersInView, setTotalPostersInView] = useState(0);
-    const [viewedPosters, setViewPosters] = useState(0);
-    const [containerWidth, setContainerWidth]  = useState(0);
-    const [posterIndex, setPosterIndex] = useState(0);
-    const [rowPadding, setRowPadding] = useState(0);
-    const [genres, setGenres] = useState([]);
-    const [delayMount, setDelayMount] = useState(false);
-    const [delayHandler, setDelayHandler] = useState(null);
-    const [cardPopupBackdrop, setCardPopupBackdrop] = useState('');
-    const [cardPopupAirDate, setCardPopupAirDate] = useState(null);
-    const [cardPopupRating, setCardPopupRating] = useState(null);
-    const [cardPopupTitle, setCardPopupTitle] = useState('');
-    const [postersInViewTabNumber, setPostersInViewTabNumber] = useState(0);
-    const [posterWidth, setPosterWidth] = useState(0);
+    // const[isHovered, setIsHovered] = useState(false);
+    // const [cardPopupWidth, setCardPopupWidth] = useState(''); 
+    // const [cardPopupHeight, setCardPopupHeight] = useState('');
+    // const [distance, setDistance] = useState(0);
+    // const [totalPostersInView, setTotalPostersInView] = useState(0);
+    // const [viewedPosters, setViewPosters] = useState(0);
+    // const [containerWidth, setContainerWidth]  = useState(0);
+    // const [posterIndex, setPosterIndex] = useState(0);
+    // const [rowPadding, setRowPadding] = useState(0);
+    // const [genres, setGenres] = useState([]);
+    // const [delayMount, setDelayMount] = useState(false);
+    // const [delayHandler, setDelayHandler] = useState(null);
+    // const [cardPopupBackdrop, setCardPopupBackdrop] = useState('');
+    // const [cardPopupAirDate, setCardPopupAirDate] = useState(null);
+    // const [cardPopupRating, setCardPopupRating] = useState(null);
+    // const [cardPopupTitle, setCardPopupTitle] = useState('');
+    // const [postersInViewTabNumber, setPostersInViewTabNumber] = useState(0);
+    // const [posterWidth, setPosterWidth] = useState(0);
 
-    const popupProps = {
+    // const popupProps = {
 
-        setIsHovered, isHovered,
-        setCardPopupWidth,setCardPopupHeight,
-        distance, setDistance,
-        posterIndex, setPosterIndex,
-        totalPostersInView, setTotalPostersInView,
-        setRowPadding, rowPadding,
-        viewedPosters, setViewPosters,
-        containerWidth, setContainerWidth,
-        setGenres, genres,
-        cardPopupWidth, cardPopupHeight,
-        delayMount, setDelayMount,
-        delayHandler, setDelayHandler,
-        cardPopupBackdrop, setCardPopupBackdrop,
-        cardPopupRating, setCardPopupRating,
-        cardPopupTitle, setCardPopupTitle,
-        postersInViewTabNumber, setPostersInViewTabNumber,
-        cardPopupAirDate, setCardPopupAirDate,
-        posterWidth, setPosterWidth
-    };
+    //     setIsHovered, isHovered,
+    //     setCardPopupWidth,setCardPopupHeight,
+    //     distance, setDistance,
+    //     posterIndex, setPosterIndex,
+    //     totalPostersInView, setTotalPostersInView,
+    //     setRowPadding, rowPadding,
+    //     viewedPosters, setViewPosters,
+    //     containerWidth, setContainerWidth,
+    //     setGenres, genres,
+    //     cardPopupWidth, cardPopupHeight,
+    //     delayMount, setDelayMount,
+    //     delayHandler, setDelayHandler,
+    //     cardPopupBackdrop, setCardPopupBackdrop,
+    //     cardPopupRating, setCardPopupRating,
+    //     cardPopupTitle, setCardPopupTitle,
+    //     postersInViewTabNumber, setPostersInViewTabNumber,
+    //     cardPopupAirDate, setCardPopupAirDate,
+    //     posterWidth, setPosterWidth
+    // };
 
 
     return (
